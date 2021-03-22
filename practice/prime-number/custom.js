8b3d76032ee99f1f3ef66dc186156d4cf7871b91
Vum <ldxl3284@gmail.com>
2021-03-22 13:56:43 +0800

    '3/22practice'

--- practice/prime-number/custom.js
+++ practice/prime-number/custom.js
@@ -7,10 +7,12 @@ for (let dividend = 2; dividend <= 10; dividend++) {
     for (let dividsor = 2; dividsor < dividend; dividsor++) {
         if (dividend % dividsor == 0) {
             isPrime = false;
+            // 一個等於 就是宣告 isPrime是false 這件事。
         }
     }
 
     if (isPrime == false) {
+        // 用 == 表示用來判斷 "isPrime為false" 這件事是不是對的。
         console.log(dividend + ' not prime.')
     }
     else {
